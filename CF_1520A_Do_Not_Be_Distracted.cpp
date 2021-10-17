b52b56ba8d78937382e2d7ff69d1a8397fd4d734
Ayush Singh <ayushsnh711@gmail.com>
2021-10-17 08:15:18 +0530

    Added a solution to Codeforces Round #719 Div 3 A

---
+++ CF_1520A_Do_Not_Be_Distracted.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define mn 10000007
+using namespace std;
+void koli()
+ {
+   int n;cin>>n;
+   string s;
+   cin>>s;
+   if(n==1||n==2){cout<<"YES"<<endl;}
+   else
+   {int cnt(0);
+   for(int i=0;i<n;i++)
+   {
+     if(s[i]!=s[i+1])
+     {cnt=count(s.begin()+i+1,s.end(),s[i]); 
+     }if(cnt>=1){cout<<"NO"<<endl;break;}
+   }if(cnt==0){cout<<"YES"<<endl;}
+   }
+ 
+ }
+int main()
+{
+  int t;
+  cin>>t;
+  while(t--)
+  {koli();}
+ 
+return 0;
+ 
+}
