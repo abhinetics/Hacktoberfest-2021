18e551d38224657bd58419a306b0359913a239f7
Shweta <2002shwetarani@gmail.com>
2021-10-24 12:33:50 +0530

    Created LAS Problem

--- LAS_problem.cpp
+++ LAS_problem.cpp
@@ -15,6 +15,7 @@ int util(vector<int> const &A, int start, int end, bool flag)
         else if (!flag && A[i - 1] > A[i]) {
             result = max(result, 1 + util(A, i + 1, end, !flag));
         }
+ 
         else {
             result = max(result, util(A, i + 1, end, flag));
         }
@@ -24,7 +25,7 @@ int util(vector<int> const &A, int start, int end, bool flag)
 }
  
 // Function to find the length of the longest subsequence with alternate
-// low and high elements. It calls the `util()` method.
+// low and high elements
 int findLongestSequence(vector<int> const &A)
 {
     int n = A.size();
@@ -35,16 +36,14 @@ int findLongestSequence(vector<int> const &A)
     }
  
     // Fix the first element and recur for the remaining elements as the first
-    // element will always be part of the longest subsequence
- 
+    // element will always be part of the longest subsequence (why?)
+
     return 1 + max(util(A, 1, n - 1, true), util(A, 1, n - 1, false));
- 
-}
- 
+ }
 int main()
 {
     vector<int> A = { 8, 9, 6, 4, 5, 7, 3, 2, 4 };
-
+ 
     cout << "The length of the longest alternating subsequence is "
          << findLongestSequence(A);
  
